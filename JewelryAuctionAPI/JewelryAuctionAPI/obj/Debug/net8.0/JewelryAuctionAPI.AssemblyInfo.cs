42ca96a9921d0a3910259dd79b492f63e79c1f53
Senuki Perera <109462298+SenukiPerera@users.noreply.github.com>
2024-10-19 10:52:39 +0530

    auction

--- JewelryAuctionAPI/JewelryAuctionAPI/obj/Debug/net8.0/JewelryAuctionAPI.AssemblyInfo.cs
+++ JewelryAuctionAPI/JewelryAuctionAPI/obj/Debug/net8.0/JewelryAuctionAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JewelryAuctionAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6828a551edc28c67ab1c26d15124aeb8ede86550")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5cad4f3a2c2d815aa67937db675c3125cb7a5e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("JewelryAuctionAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JewelryAuctionAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
