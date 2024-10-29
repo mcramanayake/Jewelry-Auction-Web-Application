36284bd54daa90c76ce949df1995ef4b94053507
A P H D S Sanjula <137377477+ssanjula2@users.noreply.github.com>
2024-10-29 21:25:42 +0530

    Update App.js

--- jewelry-auction-frontend/src/App.js
+++ jewelry-auction-frontend/src/App.js
@@ -3,79 +3,82 @@
 //import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginSignup from './components/LoginSignUp/LoginSignup';
 import Login from './components/LoginSignUp/Login';
-import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import MyAccount from './components/Profile/MyAccount';
 import MyBids from './components/Profile/MyBids';
-import Payments from './components/Profile/Payments'; 
+import Payments from './components/Profile/Payments';
 import Winnings from './components/Profile/Winnings';
 import ErrorBoundary from './components/ErrorBoundary';
 import NotFound from './components/NotFound';
 import HomePage from './components/Admin&Home/HomePage';
+import Auctions from './components/Auctions/Auctions';
 import AddItemPage from './components/Admin&Home/AddItemPage';
 import AdminDashboard from './components/Admin&Home/AdminDashboard';
-import SellWithUs from "./components/SellWithUs&ContactUs&AboutUs/SellWithUs";
-import Contact from "./components/SellWithUs&ContactUs&AboutUs/Contact";
-import AboutUs from "./components/SellWithUs&ContactUs&AboutUs/AboutUs";
+import SellWithUs from './components/SellWithUs&ContactUs&AboutUs/SellWithUs';
+import Contact from './components/SellWithUs&ContactUs&AboutUs/Contact';
+import AboutUs from './components/SellWithUs&ContactUs&AboutUs/AboutUs';
 
 function App() {
+    const route = createBrowserRouter([
+        {
+            path: '/',
+            element: <LoginSignup />,
+        },
+        {
+            path: '/login',
+            element: <Login />,
+        },
+        {
+            path: '/MyAccount',
+            element: <MyAccount />,
+        },
+        {
+            path: '/MyBids',
+            element: <MyBids />,
+        },
+        {
+            path: '/Payments',
+            element: <Payments />,
+        },
+        {
+            path: '/Winnings',
+            element: <Winnings />,
+        },
+        {
+            path: '/HomePage',
+            element: <HomePage />,
+        },
+        {
+            path: '/auctions',
+            element: <Auctions />,
+        },
+        {
+            path: '/AddItemPage',
+            element: <AddItemPage />,
+        },
+        {
+            path: '/AdminDashboard',
+            element: <AdminDashboard />,
+        },
+        {
+            path: '/sell-with-us',
+            element: <SellWithUs />,
+        },
+        {
+            path: '/contact-us',
+            element: <Contact />,
+        },
+        {
+            path: '/aboutus',
+            element: <AboutUs />,
+        },
+        {
+            path: '*', // This will catch all undefined routes
+            element: <NotFound />, // Render NotFound component for unknown routes
+        },
+    ]);
 
-  const route = createBrowserRouter ([
-    {
-      path:"/",
-      element:<LoginSignup />,
-    },
-    {
-      path:"/login",
-      element:<Login />,
-    },
-    {
-      path: "/MyAccount",
-      element: <MyAccount />,
-    },
-    {
-      path: "/MyBids",
-      element:<MyBids />
-    },
-    {
-      path: "/Payments",
-      element: <Payments />
-    },
-    {
-      path: "/Winnings",
-      element:<Winnings />
-    },
-    {
-      path: "/HomePage",
-      element:<HomePage />
-    },
-    {
-      path: "/AddItemPage",
-      element:<AddItemPage />
-    },
-    {
-      path: "/AdminDashboard",
-      element:<AdminDashboard />
-    },
-    {
-      path:"/sell-with-us", 
-      element:<SellWithUs />
-    },
-    {
-      path:"/contact-us", 
-      element:<Contact />
-    },
-    {
-      path:"/aboutus", 
-      element:<AboutUs />
-    },
-    {
-      path: "*", // This will catch all undefined routes
-      element: <NotFound />, // Render NotFound component for unknown routes
-    }
-  ]);
-
-
- /* const [user, setUser] = useState(null);
+    /* const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -93,18 +96,16 @@ function App() {
 
   if (loading) return <div>Loading...</div>; */
 
+    //const isAdmin = user?.role === 'admin';
 
- // const isAdmin = user?.role === 'admin';
-  
-  return (
+    return (
+        <ErrorBoundary>
+            <div>
+                <RouterProvider router={route}></RouterProvider>
+            </div>
+        </ErrorBoundary>
 
-    <ErrorBoundary>
-      <div>
-        <RouterProvider router={route}></RouterProvider>
-      </div>
-    </ErrorBoundary>
-
-   /* <Router>
+        /* <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage setUser={setUser} />} />
@@ -115,7 +116,7 @@ function App() {
 
       </Routes>
     </Router> */
-  );
+    );
 }
 
 export default App;
