c7c075a86e75cc943a86952e2deb95d54bb7a1fd
Senuki Perera <109462298+SenukiPerera@users.noreply.github.com>
2024-10-30 14:04:35 +0530

    navbar & footer

--- jewelry-auction-frontend/src/components/SellWithUs&ContactUs&AboutUs/SellWithUs.jsx
+++ jewelry-auction-frontend/src/components/SellWithUs&ContactUs&AboutUs/SellWithUs.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./SellWithUs.css";
-import yourImage from "./Images/SellWithUs.png";
+import "../Profile/Navbar.css";
+import { FaBars, FaCircleUser } from 'react-icons/fa6';
+import { Link } from 'react-router-dom';
 
 export default class SellWithUs extends Component {
   state = {
@@ -9,6 +11,11 @@ export default class SellWithUs extends Component {
     message: "",
     filePath: "",
     errors: {},
+    isOpen: false, // Added state for menu toggle
+  };
+
+  toggleMenu = () => {
+    this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
   };
 
   handleInputChange = (e) => {
@@ -81,16 +88,53 @@ export default class SellWithUs extends Component {
   };
 
   render() {
-    const { errors } = this.state;
+    const { errors, isOpen } = this.state;
 
     return (
       <div>
-        <div className="sell-with-us-section">
-          <div className="image-container">
-            <img src={yourImage} alt="Sell With Us" />
-            <div className="overlay-text">Sell With Us</div>
+        {/*Navbar*/}
+        <div className="navbar">
+          <div className="nav-logo">
+            <p>
+              <Link to="/HomePage" className="logo-home">
+                The Auction Room
+              </Link>
+            </p>
+          </div>
+          <ul className={`nav-menu ${isOpen ? 'nav-menu-active' : ''}`}>
+            <li>Auctions</li>
+            <li>
+              <Link to="/sell-with-us" className="user-nav">
+                Sell with us
+              </Link>
+            </li>
+            <li>
+              <Link to="/aboutus" className="user-nav">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact-us" className="user-nav">
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <div className="nav-login-user">
+            <Link to="/MyAccount" className="user-nav">
+              <FaCircleUser size={25} />
+            </Link>
+
+            <div className="nav-icon" onClick={this.toggleMenu}>
+              <FaBars size={25} />
+            </div>
           </div>
         </div>
+
+        <div className="sell-with-us-section">
+                <h1>Sell With Us</h1>
+        </div>
+
+
 
         <div className="responsive-paragraph">
           <span className="xyz">
@@ -158,6 +202,29 @@ export default class SellWithUs extends Component {
           </form>
         </section>
         <br />
+        {/* Footer */}
+        <div className="footer">
+                <div className="first">
+                    <h1>The Auction Room</h1>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+                <div className="second">
+                    <ul>
+                        <li>Auctions</li>
+                        <li>Past Auctions</li>
+                        <li>Profile</li>
+                        <li>Sell with us</li>
+                    </ul>
+                </div>
+                <div className="third">
+                    <ul>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li>Terms & Conditions</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+            </div>
       </div>
     );
   }
